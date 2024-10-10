e10bd6e75302c4d9eb43dc9d8b3c35345f9b0df9
Anton Vlezko <anton@vlezko.com>
2024-10-10 19:53:32 +0700

    Small resources fixes

--- HelloWorldApp/Resources/Derived/Sources/TuistAssets+Resources.swift
+++ HelloWorldApp/Resources/Derived/Sources/TuistAssets+Resources.swift
@@ -1,7 +1,5 @@
 // swiftlint:disable all
-// swift-format-ignore-file
-// swiftformat:disable all
-// Generated using tuist — https://github.com/tuist/tuist
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
 
 #if os(macOS)
   import AppKit
@@ -14,34 +12,41 @@
   import SwiftUI
 #endif
 
+// Deprecated typealiases
+@available(*, deprecated, renamed: "ColorAsset.Color", message: "This typealias will be removed in SwiftGen 7.0")
+public typealias AssetColorTypeAlias = ColorAsset.Color
+@available(*, deprecated, renamed: "ImageAsset.Image", message: "This typealias will be removed in SwiftGen 7.0")
+public typealias AssetImageTypeAlias = ImageAsset.Image
+
 // swiftlint:disable superfluous_disable_command file_length implicit_return
 
 // MARK: - Asset Catalogs
 
 // swiftlint:disable identifier_name line_length nesting type_body_length type_name
-public enum ResourcesAsset {
-  public static let accentColor = ResourcesColors(name: "Accent Color")
-  public static let buttonBackgroundColor = ResourcesColors(name: "Button Background Color")
-  public static let buttonTextColor = ResourcesColors(name: "Button Text Color")
-  public static let surfaceColor = ResourcesColors(name: "Surface Color")
-  public static let surfaceSecondaryColor = ResourcesColors(name: "Surface Secondary Color")
-  public static let textPrimaryColor = ResourcesColors(name: "Text Primary Color")
-  public static let textSecondaryColor = ResourcesColors(name: "Text Secondary Color")
+public enum Asset {
+  public static let accentColor = ColorAsset(name: "Accent Color")
+  public static let buttonBackgroundColor = ColorAsset(name: "Button Background Color")
+  public static let buttonTextColor = ColorAsset(name: "Button Text Color")
+  public static let surfaceColor = ColorAsset(name: "Surface Color")
+  public static let surfaceSecondaryColor = ColorAsset(name: "Surface Secondary Color")
+  public static let textPrimaryColor = ColorAsset(name: "Text Primary Color")
+  public static let textSecondaryColor = ColorAsset(name: "Text Secondary Color")
+  public static let hackerman = ImageAsset(name: "hackerman")
 }
 // swiftlint:enable identifier_name line_length nesting type_body_length type_name
 
 // MARK: - Implementation Details
 
-public final class ResourcesColors {
+public final class ColorAsset {
   public fileprivate(set) var name: String
 
   #if os(macOS)
   public typealias Color = NSColor
-  #elseif os(iOS) || os(tvOS) || os(watchOS) || os(visionOS)
+  #elseif os(iOS) || os(tvOS) || os(watchOS)
   public typealias Color = UIColor
   #endif
 
-  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, visionOS 1.0, *)
+  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, *)
   public private(set) lazy var color: Color = {
     guard let color = Color(asset: self) else {
       fatalError("Unable to load color asset named \(name).")
@@ -49,21 +54,22 @@ public final class ResourcesColors {
     return color
   }()
 
-  #if canImport(SwiftUI)
-  private var _swiftUIColor: Any? = nil
-  @available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, visionOS 1.0, *)
-  public private(set) var swiftUIColor: SwiftUI.Color {
-    get {
-      if self._swiftUIColor == nil {
-        self._swiftUIColor = SwiftUI.Color(asset: self)
-      }
-
-      return self._swiftUIColor as! SwiftUI.Color
+  #if os(iOS) || os(tvOS)
+  @available(iOS 11.0, tvOS 11.0, *)
+  public func color(compatibleWith traitCollection: UITraitCollection) -> Color {
+    let bundle = Bundle.module
+    guard let color = Color(named: name, in: bundle, compatibleWith: traitCollection) else {
+      fatalError("Unable to load color asset named \(name).")
     }
-    set {
-      self._swiftUIColor = newValue
-    }
+    return color
   }
+  #endif
+
+  #if canImport(SwiftUI)
+  @available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
+  public private(set) lazy var swiftUIColor: SwiftUI.Color = {
+    SwiftUI.Color(asset: self)
+  }()
   #endif
 
   fileprivate init(name: String) {
@@ -71,11 +77,11 @@ public final class ResourcesColors {
   }
 }
 
-public extension ResourcesColors.Color {
-  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, visionOS 1.0, *)
-  convenience init?(asset: ResourcesColors) {
+public extension ColorAsset.Color {
+  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, *)
+  convenience init?(asset: ColorAsset) {
     let bundle = Bundle.module
-    #if os(iOS) || os(tvOS) || os(visionOS)
+    #if os(iOS) || os(tvOS)
     self.init(named: asset.name, in: bundle, compatibleWith: nil)
     #elseif os(macOS)
     self.init(named: NSColor.Name(asset.name), bundle: bundle)
@@ -86,14 +92,92 @@ public extension ResourcesColors.Color {
 }
 
 #if canImport(SwiftUI)
-@available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, visionOS 1.0, *)
+@available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
 public extension SwiftUI.Color {
-  init(asset: ResourcesColors) {
+  init(asset: ColorAsset) {
     let bundle = Bundle.module
     self.init(asset.name, bundle: bundle)
   }
 }
 #endif
 
-// swiftlint:enable all
-// swiftformat:enable all
+public struct ImageAsset {
+  public fileprivate(set) var name: String
+
+  #if os(macOS)
+  public typealias Image = NSImage
+  #elseif os(iOS) || os(tvOS) || os(watchOS)
+  public typealias Image = UIImage
+  #endif
+
+  @available(iOS 8.0, tvOS 9.0, watchOS 2.0, macOS 10.7, *)
+  public var image: Image {
+    let bundle = Bundle.module
+    #if os(iOS) || os(tvOS)
+    let image = Image(named: name, in: bundle, compatibleWith: nil)
+    #elseif os(macOS)
+    let name = NSImage.Name(self.name)
+    let image = (bundle == .main) ? NSImage(named: name) : bundle.image(forResource: name)
+    #elseif os(watchOS)
+    let image = Image(named: name)
+    #endif
+    guard let result = image else {
+      fatalError("Unable to load image asset named \(name).")
+    }
+    return result
+  }
+
+  #if os(iOS) || os(tvOS)
+  @available(iOS 8.0, tvOS 9.0, *)
+  public func image(compatibleWith traitCollection: UITraitCollection) -> Image {
+    let bundle = Bundle.module
+    guard let result = Image(named: name, in: bundle, compatibleWith: traitCollection) else {
+      fatalError("Unable to load image asset named \(name).")
+    }
+    return result
+  }
+  #endif
+
+  #if canImport(SwiftUI)
+  @available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
+  public var swiftUIImage: SwiftUI.Image {
+    SwiftUI.Image(asset: self)
+  }
+  #endif
+}
+
+public extension ImageAsset.Image {
+  @available(iOS 8.0, tvOS 9.0, watchOS 2.0, *)
+  @available(macOS, deprecated,
+    message: "This initializer is unsafe on macOS, please use the ImageAsset.image property")
+  convenience init?(asset: ImageAsset) {
+    #if os(iOS) || os(tvOS)
+    let bundle = Bundle.module
+    self.init(named: asset.name, in: bundle, compatibleWith: nil)
+    #elseif os(macOS)
+    self.init(named: NSImage.Name(asset.name))
+    #elseif os(watchOS)
+    self.init(named: asset.name)
+    #endif
+  }
+}
+
+#if canImport(SwiftUI)
+@available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
+public extension SwiftUI.Image {
+  init(asset: ImageAsset) {
+    let bundle = Bundle.module
+    self.init(asset.name, bundle: bundle)
+  }
+
+  init(asset: ImageAsset, label: Text) {
+    let bundle = Bundle.module
+    self.init(asset.name, bundle: bundle, label: label)
+  }
+
+  init(decorative asset: ImageAsset) {
+    let bundle = Bundle.module
+    self.init(decorative: asset.name, bundle: bundle)
+  }
+}
+#endif
