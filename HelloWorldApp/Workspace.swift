36449ff1a8a36e641629cfa79a01f52661ccf4d7
Anton Vlezko <anton@vlezko.com>
2024-10-18 14:22:22 +0700

    Add new scheme

--- HelloWorldApp/Workspace.swift
+++ HelloWorldApp/Workspace.swift
@@ -2,28 +2,29 @@ import ProjectDescription
 import ProjectDescriptionHelpers
 
 // https://github.com/tuist/tuist/tree/main/fixtures/ios_app_with_custom_scheme
-//let customFrameworkScheme: Scheme = .scheme(
-//    name: "Workspace-Framework",
-//    shared: true,
-//    buildAction: .buildAction(
-//        targets: [
-//            generateTargetReference(name: .CommonApplication)
-//        ],
-//        preActions: []
-//    ),
-//    testAction: TestAction
-//        .targets([
-//            .testableTarget(
-//                target: generateTestTargetReference(name: .CommonApplication)
-//            )
-//        ]),
-//    archiveAction: .archiveAction(configuration: "Debug", customArchiveName: "Something2")
-//)
+let customFrameworkScheme: Scheme = .scheme(
+    name: "Workspace-Framework",
+    shared: true,
+    buildAction: .buildAction(
+        targets: [
+            .project(path: "App", target: "App")
+        ],
+        preActions: []
+    ),
+    testAction: .targets([
+        .testableTarget(
+            target: .project(path: "App", target: "AppTests")
+        )
+    ]),
+    runAction: .runAction(executable: .project(path: "App", target: "App")),
+    archiveAction: .archiveAction(configuration: "Debug", customArchiveName: "Something2")
+)
 
 let workspace = Workspace(
     name: Constants.projectName,
     projects: [
         "App/**",
         "Frameworks/**"
-    ]
+    ],
+    schemes: [customFrameworkScheme]
 )
