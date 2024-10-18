5b82aea5fda796532646c58e10b52cfad585548b
Anton Vlezko <anton@vlezko.com>
2024-10-18 13:58:28 +0700

    Add test fixes

--- HelloWorldApp/App/Tests/AppTests.swift
+++ HelloWorldApp/App/Tests/AppTests.swift
@@ -1,14 +1,14 @@
 //
-//  HelloWorldTests.swift
-//  HelloWorldTests
+//  AppTests.swift
+//  AppTests
 //
 //  Created by Anton Vlezko on 12/03/2024.
 //  Copyright (c) 2024 Smart Lads Software. All rights reserved.
 
 import XCTest
-//@testable import HelloWorldApp
+@testable import App
 
-final class HelloWorldAppTests: XCTestCase {
+final class AppTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
